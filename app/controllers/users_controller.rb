4d06508d46e24a809008e9078fd308b00e01286e
Nick Pomroy <npomroy69@hotmail.com>
2016-12-05 09:35:32 +0000

    Added file field for image upload to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
-class UsersController < Applicationcontroller
+class UsersController < ApplicationController
    # GET to /users/:id 
    def show
-      @user = User.find( params[:id]) 
+      @user = User.find( params[:id] ) 
    end
 end
