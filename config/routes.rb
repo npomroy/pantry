a8c56d40c9b8d2c7a12971c8d7efbb8408f355fa
Nick Pomroy <npomroy69@hotmail.com>
2016-12-05 08:46:54 +0000

    Added routes for profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do
+    resource :profile
+  end
   get 'faq', to: 'pages#faq'
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
