ca6d1fa1105953dfaa595c736b6e6bd650920ec6
Nick Pomroy <npomroy69@hotmail.com>
2016-12-06 02:27:04 +0000

    Added new food item form

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -3,5 +3,6 @@ class FoodItemsController < ApplicationController
    # GET to /users/:user_id/food_items/new
    def new
       # Render blank food item details form
+      @food_item = Food_Item.new
    end
 end
