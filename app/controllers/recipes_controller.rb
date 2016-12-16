7a5d8aa30f691f7766f61f5d8484ceb7465a8333
Nick Pomroy <npomroy69@hotmail.com>
2016-12-16 15:52:54 +0000

    Added recipe create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,20 @@ class RecipesController < ApplicationController
    def new
        @recipe = Recipe.new
    end
+   
+   def create
+      @recipe = Recipe.new(recipe_params)
+      if @recipe.save
+          flash[:success] = "Recipe created"
+          redirect_to root_path
+      else
+          flash[:error] = "Recipe creation failed"
+          render action: :new
+      end
+   end
+   
+   private
+        def recipe_params
+           params.require(:recipe).permit(:name, :prep_time, :bake_time, :instructions, :details) 
+        end
 end
